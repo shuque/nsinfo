ae24005e557cbdfd1a925bc7f6c37cb4455415f3
Shumon Huque <shuque@gmail.com>
2020-05-11 07:41:13 -0400

    Lookup resolver from resolv.conf; more cleanup.

--- nsinfo.go
+++ nsinfo.go
@@ -5,15 +5,22 @@ import (
 	"net"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/miekg/dns"
 )
 
+//
+// Print usage string
+//
 func usage() {
 	fmt.Printf("Usage: nsinfo <zonename>\n")
 }
 
+//
+// Make domain name fully qualified
+//
 func mkFqdn(qname string) string {
 	if strings.HasSuffix(qname, ".") {
 		return qname
@@ -21,19 +28,25 @@ func mkFqdn(qname string) string {
 	return qname + "."
 }
 
-func doQuery(qname string, qtype uint16) (response *dns.Msg, err error) {
+//
+// Perform DNS lookup and return response message
+//
+func doQuery(resolver string, qname string, qtype uint16) (response *dns.Msg, err error) {
 
 	m := new(dns.Msg)
 	m.Id = dns.Id()
 	m.RecursionDesired = true
-	// m.SetEdns0(4096, true)
+	m.SetEdns0(1460, true)
 	m.Question = make([]dns.Question, 1)
 	m.Question[0] = dns.Question{Name: qname, Qtype: qtype, Qclass: dns.ClassINET}
 
 	c := new(dns.Client)
-	response, _, err = c.Exchange(m, "127.0.0.1:53")
+	response, _, err = c.Exchange(m, addressString(resolver, 53))
 	if err != nil {
 		return nil, err
+	}
+	if response == nil {
+		return nil, fmt.Errorf("Error: null DNS response to query")
 	}
 	switch response.MsgHdr.Rcode {
 	case dns.RcodeSuccess:
@@ -53,13 +66,15 @@ func doQuery(qname string, qtype uint16) (response *dns.Msg, err error) {
 	}
 	if rrcount == 0 {
 		return nil, fmt.Errorf("NODATA: %s/%d", qname, qtype)
-
 	}
 
 	return response, err
 }
 
-func getIPAddresses(hostname string) []net.IP {
+//
+// Obtain list of IPv4 and IPv6 addresses for hostname
+//
+func getIPAddresses(resolver string, hostname string) []net.IP {
 
 	var ipList []net.IP
 
@@ -69,12 +84,8 @@ func getIPAddresses(hostname string) []net.IP {
 	}
 
 	for _, rrtype := range rrTypes {
-
-		response, err := doQuery(hostname, rrtype)
+		response, err := doQuery(resolver, hostname, rrtype)
 		if err != nil {
-			break
-		}
-		if response == nil {
 			break
 		}
 		for _, rr := range response.Answer {
@@ -92,14 +103,17 @@ func getIPAddresses(hostname string) []net.IP {
 
 }
 
-func reverseLookup(ipaddr net.IP) string {
+//
+// Return reverse DNS lookup of given IP address
+//
+func reverseLookup(resolver string, ipaddr net.IP) string {
 
 	arpaname, err := dns.ReverseAddr(ipaddr.String())
 	if err != nil {
 		return ""
 	}
-	response, err := doQuery(arpaname, dns.TypePTR)
-	if err != nil || response == nil || len(response.Answer) < 1 {
+	response, err := doQuery(resolver, arpaname, dns.TypePTR)
+	if err != nil || len(response.Answer) < 1 {
 		return "NO-PTR"
 	}
 	ptrRr := response.Answer[0].(*dns.PTR)
@@ -111,7 +125,7 @@ const hexDigit = "0123456789abcdef"
 var ip2asnSuffixV4 = "origin.asn.cymru.com."
 var ip2asnSuffixV6 = "origin6.asn.cymru.com."
 
-func ip2asn(ip net.IP) string {
+func ip2asn(resolver string, ip net.IP) string {
 
 	var h1, h2 byte
 	var qname string
@@ -138,7 +152,7 @@ func ip2asn(ip net.IP) string {
 		qname = string(buf6) + ip2asnSuffixV6
 	}
 
-	response, _ := doQuery(qname, dns.TypeTXT)
+	response, _ := doQuery(resolver, qname, dns.TypeTXT)
 	if response == nil {
 		fmt.Printf("No ASN found\n")
 		return ""
@@ -149,6 +163,30 @@ func ip2asn(ip net.IP) string {
 
 }
 
+//
+// addressString() - return address:port string
+//
+func addressString(addr string, port int) string {
+	if strings.Index(addr, ":") == -1 {
+		return addr + ":" + strconv.Itoa(port)
+	}
+	return "[" + addr + "]" + ":" + strconv.Itoa(port)
+}
+
+//
+// getSysResolver() - obtain (1st) system default resolver address
+//
+func getSysResolver() (resolver string, err error) {
+	config, err := dns.ClientConfigFromFile("/etc/resolv.conf")
+	if err == nil {
+		resolver = config.Servers[0]
+	}
+	return
+}
+
+//
+// main()
+//
 func main() {
 
 	var err error
@@ -157,15 +195,16 @@ func main() {
 		usage()
 		return
 	}
-	zone := mkFqdn(os.Args[1])
 
-	response, err := doQuery(zone, dns.TypeNS)
+	zone := mkFqdn(os.Args[1])
+	resolver, err := getSysResolver()
+	if err != nil {
+		fmt.Printf("Error obtaining resolver address: %s", err.Error())
+	}
+
+	response, err := doQuery(resolver, zone, dns.TypeNS)
 	if err != nil {
 		fmt.Printf("%s\n", err.Error())
-		return
-	}
-	if response == nil {
-		fmt.Printf("Error: null DNS response to NS query")
 		return
 	}
 
@@ -179,16 +218,16 @@ func main() {
 		}
 	}
 
-	// TODO: sort this in DNS canonical order
+	// TODO: sort in DNS canonical order
 	sort.Strings(nsNames)
 
 	var ipList []net.IP
 
 	for _, nsName := range nsNames {
-		ipList = getIPAddresses(nsName)
+		ipList = getIPAddresses(resolver, nsName)
 		for _, ip := range ipList {
-			fmt.Printf("%s %s %s %s\n", nsName, ip, ip2asn(ip),
-				reverseLookup(ip))
+			fmt.Printf("%s %s %s %s\n", nsName, ip, ip2asn(resolver, ip),
+				reverseLookup(resolver, ip))
 		}
 	}
 
